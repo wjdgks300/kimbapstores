badb76b80072feb4ae082992ff591c2553b276b1
lee <wjdgks300@naver.com>
2020-12-29 18:04:43 +0900

    unity_practice

---
+++ Assets/script/portalteleport.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class portalteleport : MonoBehaviour
+{ 
+    public Transform teleportTarget;
+    public GameObject thePlayer;
+
+    Collider m_objectCollider;
+    public void triggeron()
+    {
+        m_objectCollider = GetComponent<Collider>();
+        m_objectCollider.isTrigger = true;
+    }
+
+    public void triggeroff()
+    {
+        m_objectCollider = GetComponent<Collider>();
+        m_objectCollider.isTrigger = false;
+    }
+    private void OnTriggerStay(Collider other)
+    {
+        thePlayer.transform.position = teleportTarget.transform.position;
+    }
+    
+}
